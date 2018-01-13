2fc334848e2fe50eac16558542bd73b58c0710ec
Joe <jverderber@keyholesoftware.com>
2018-01-13 12:36:47 -0600

    add-project-from-start.spring.io

---
+++ src/main/java/com/khs/springbootmicroserver/SpringBootMicroserverApplication.java
@@ -0,0 +1,12 @@
+package com.khs.springbootmicroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserverApplication.class, args);
+	}
+}
